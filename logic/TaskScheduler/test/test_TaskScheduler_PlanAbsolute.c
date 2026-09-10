6a904524395a026e7bea6ec6592d699a70c48dc8
Szejgu <s.szewczyk02@gmail.com>
2026-09-10 18:07:07 +0200

    expanded Plan Absolute tests

--- logic/TaskScheduler/test/test_TaskScheduler_PlanAbsolute.c
+++ logic/TaskScheduler/test/test_TaskScheduler_PlanAbsolute.c
@@ -146,4 +146,218 @@ void test_TaskScheduler_PlanAbsolute_fixed_hour_next_day(void)
 
 }
 
+void test_TaskScheduler_PlanAbsolute_plan_next_occurence_after_it_happened(void)
+{
+    //ARRANGE
+    TS_TimeStruct_t TimeInput = {.data = {
+        .year = 0,
+        .day = 0,
+        .month = 0,
+        .hour = 16,
+        .minute = 0,
+        .second = 0,
+        .milisecond = 0
+        }
+    };
+    
+    HWTimestampRet.data.year = 24;
+    HWTimestampRet.data.day = 20;
+    HWTimestampRet.data.month = 9;
+    HWTimestampRet.data.hour = 16;
+    HWTimestampRet.data.minute = 0;
+    HWTimestampRet.data.second = 0;
+    HWTimestampRet.data.milisecond = 0;
+
+    TS_TimeStruct_t TimeReturnExpected = {.data = {
+        .year = 0,
+        .day = 1,
+        .month = 0,
+        .hour = 0,
+        .minute = 0,
+        .second = 0,
+        .milisecond = 0
+        }
+    };
+
+    TS_TimeStruct_t TimeReturnActual = {0};
+
+    //ACT
+    TS_InitStruct_t initStruct = goodInit();
+    TS_InitErrorCodes_t result = TS_Init(&initStruct);
+
+    TimeReturnActual = TS_PlanAbsolute(TimeInput);
+ 
+
+    //ASSERT
+    TEST_ASSERT_EQUAL(TS_InitErrorCodes_noError, result);
+    TEST_ASSERT_EQUAL(1, HWTimestampGetCallCounter);
+    TEST_ASSERT_EQUAL(TimeReturnExpected.raw, TimeReturnActual.raw);
+    
+
+}
+
+void test_TaskScheduler_PlanAbsolute_plan_next_fixed_hour_with_minutes(void)
+{
+    //ARRANGE
+    TS_TimeStruct_t TimeInput = {.data = {
+        .year = 0,
+        .day = 0,
+        .month = 0,
+        .hour = 10,
+        .minute = 30,
+        .second = 0,
+        .milisecond = 0
+        }
+    };
+    
+    HWTimestampRet.data.year = 24;
+    HWTimestampRet.data.day = 20;
+    HWTimestampRet.data.month = 9;
+    HWTimestampRet.data.hour = 9;
+    HWTimestampRet.data.minute = 56;
+    HWTimestampRet.data.second = 30;
+    HWTimestampRet.data.milisecond = 657;
+
+    TS_TimeStruct_t TimeReturnExpected = {.data = {
+        .year = 0,
+        .day = 0,
+        .month = 0,
+        .hour = 0,
+        .minute = 33,
+        .second = 29,
+        .milisecond = 342
+        }
+    };
+
+    TS_TimeStruct_t TimeReturnActual = {0};
+
+    //ACT
+    TS_InitStruct_t initStruct = goodInit();
+    TS_InitErrorCodes_t result = TS_Init(&initStruct);
+
+    TimeReturnActual = TS_PlanAbsolute(TimeInput);
+ 
+
+    //ASSERT
+    TEST_ASSERT_EQUAL(TS_InitErrorCodes_noError, result);
+    TEST_ASSERT_EQUAL(1, HWTimestampGetCallCounter);
+    TEST_ASSERT_EQUAL(TimeReturnExpected.raw, TimeReturnActual.raw);
+    
+
+}
+
+void test_TaskScheduler_PlanAbsolute_invalid_input_day(void)
+{
+    //ARRANGE
+    TS_TimeStruct_t TimeInput = {.data = {
+        .year = 0,
+        .day = 4,
+        .month = 0,
+        .hour = 10,
+        .minute = 30,
+        .second = 0,
+        .milisecond = 0
+        }
+    };
+    
+    HWTimestampRet.data.year = 24;
+    HWTimestampRet.data.day = 20;
+    HWTimestampRet.data.month = 9;
+    HWTimestampRet.data.hour = 9;
+    HWTimestampRet.data.minute = 56;
+    HWTimestampRet.data.second = 30;
+    HWTimestampRet.data.milisecond = 657;
+
+    TS_TimeStruct_t TimeReturnActual = {0};
+
+    //ACT
+    TS_InitStruct_t initStruct = goodInit();
+    TS_InitErrorCodes_t result = TS_Init(&initStruct);
+
+    TimeReturnActual = TS_PlanAbsolute(TimeInput);
+ 
+
+    //ASSERT
+    TEST_ASSERT_EQUAL(TS_InitErrorCodes_noError, result);
+    TEST_ASSERT_EQUAL(UINT64_MAX, TimeReturnActual.raw);
+    
+
+}
+
+void test_TaskScheduler_PlanAbsolute_invalid_input_month(void)
+{
+    //ARRANGE
+    TS_TimeStruct_t TimeInput = {.data = {
+        .year = 0,
+        .day = 4,
+        .month = 2,
+        .hour = 10,
+        .minute = 30,
+        .second = 0,
+        .milisecond = 0
+        }
+    };
+    
+    HWTimestampRet.data.year = 24;
+    HWTimestampRet.data.day = 20;
+    HWTimestampRet.data.month = 9;
+    HWTimestampRet.data.hour = 9;
+    HWTimestampRet.data.minute = 56;
+    HWTimestampRet.data.second = 30;
+    HWTimestampRet.data.milisecond = 657;
+
+    TS_TimeStruct_t TimeReturnActual = {0};
+
+    //ACT
+    TS_InitStruct_t initStruct = goodInit();
+    TS_InitErrorCodes_t result = TS_Init(&initStruct);
+
+    TimeReturnActual = TS_PlanAbsolute(TimeInput);
+ 
+
+    //ASSERT
+    TEST_ASSERT_EQUAL(TS_InitErrorCodes_noError, result);
+    TEST_ASSERT_EQUAL(UINT64_MAX, TimeReturnActual.raw);
+    
+
+}
+
+void test_TaskScheduler_PlanAbsolute_invalid_input_year(void)
+{
+    //ARRANGE
+    TS_TimeStruct_t TimeInput = {.data = {
+        .year = 0,
+        .day = 4,
+        .month = 2,
+        .hour = 10,
+        .minute = 30,
+        .second = 0,
+        .milisecond = 0
+        }
+    };
+    
+    HWTimestampRet.data.year = 24;
+    HWTimestampRet.data.day = 20;
+    HWTimestampRet.data.month = 9;
+    HWTimestampRet.data.hour = 9;
+    HWTimestampRet.data.minute = 56;
+    HWTimestampRet.data.second = 30;
+    HWTimestampRet.data.milisecond = 657;
+
+    TS_TimeStruct_t TimeReturnActual = {0};
+
+    //ACT
+    TS_InitStruct_t initStruct = goodInit();
+    TS_InitErrorCodes_t result = TS_Init(&initStruct);
+
+    TimeReturnActual = TS_PlanAbsolute(TimeInput);
+ 
+
+    //ASSERT
+    TEST_ASSERT_EQUAL(TS_InitErrorCodes_noError, result);
+    TEST_ASSERT_EQUAL(UINT64_MAX, TimeReturnActual.raw);
+    
+
+}
+
 //#endif // TEST
